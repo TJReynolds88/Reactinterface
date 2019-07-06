ee09bd82484bfad518ede8425e79bd5ada13bf3b
TJReynolds88 <tj.reynolds1988@gmail.com>
2019-07-05 23:42:59 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+import AddAppointments from './AddAppointments';
+import SearchAppointments from './SearchAppointments';
+import ListAppointments from './ListAppointments';
+
+import {without} from 'lodash';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      myAppointments: [],
+      formDisplay: false,
+      orderBy: 'petName',
+      orderDir: 'asc',
+      lastIndex: 0
+    };
+
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.AddAppointment = this.AddAppointment.bind(this);
+  }
+
+  toggleForm(){
+    this.setState({
+      formDisplay: !this.state.formDisplay
+    });
+  }
+
+  AddAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    apt.aptId = this.state.lastIndex;
+    tempApts.unshift(apt);
+    this.setState({
+      myAppointments: tempApts,
+      lastIndex: this.state.lastIndex + 1
+    });
+  }
+
+  deleteAppointment(apt){
+    let tempApts = this.state.myAppointments;
+    tempApts = without(tempApts,apt);
+    
+    this.setState({
+      myAppointments: tempApts
+    });
+  }
+
+  componentDidMount() {
+    fetch('./data.json')
+    .then(Response => Response.json())
+    .then(result => {
+      const apts = result.map(item => {
+        item.aptId = this.state.lastIndex;
+        this.setState({ lastIndex: this.state.lastIndex + 1})
+        return item;
+      });
+      this.setState({
+        myAppointments: apts
+      });
+    });
+  }
+
+  render() {
+    let order;
+    let filteredApts = this.state.myAppointments;
+    if(this.state.orderDir === 'asc') {
+      order = 1;
+    } else {
+      order = -1;
+    }
+
+    filteredApts.sort((a,b) => {
+      if (a[this.state.orderBy].toLowerCase() <
+          b[this.state.orderBy].toLowerCase()
+          ) {
+            return -1 * order;
+          } else {
+            return 1 * order;
+          }
+    });
+
+
+  return (
+    <main className="page bg-white" id="petratings">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12 bg-white">
+          <div className="container">
+            <AddAppointments
+              formDisplay={this.state.formDisplay}
+              toggleForm={this.toggleForm}
+              AddAppointment = {this.AddAppointment}
+            />
+            <SearchAppointments 
+              orderBy = {this.state.orderBy}
+              orderDir = {this.state.order.orderDir}
+            />
+            <ListAppointments appointments={filteredApts}
+              deleteAppointment={this.deleteAppointment} />
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+  );
+}
+}
+
+
+
+export default App;
